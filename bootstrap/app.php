e447d059fb6e5e34f96ab9992084abd0265b0134
Teguh Siswanto <teguhsiswanto@email.unikom.ac.id>
2019-12-15 10:25:12 +0700

    setting connect to db 1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -108,7 +108,7 @@ $app->router->group([
 | in your .env file,  if not set the default location will be used
 |
 */
-$app->useStoragePath(env('APP_STORAGE', base_path() . '/storage'));
+//$app->useStoragePath(env('APP_STORAGE', base_path() . '/storage'));
 # [END]
 
 
